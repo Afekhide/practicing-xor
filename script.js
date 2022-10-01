c3f40138445dc3c12f9690fdd0a93a3f3e1ddd54
Dlite <afekhide009@gmail.com>
2022-10-01 02:26:32 +0100

    added an event listener to remove the draggble function

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ let move = (ev) => {
     let y = ev.pageY;
 
     //console.clear();
-    console.log({x,y})
+    //console.log({x,y})
 
     xor.style.top = `${(y-h/2)}px`;
     xor.style.left = `${(x-w/2)}px`;
 }
 
+
+window.addEventListener("keypress", ev => {
+    console.log(event.ctrlKey);
+    if ((ev.key === "Enter")) window.removeEventListener("mousemove", move);
+})
+window.addEventListener('dblclick', ev => window.addEventListener("mousemove", move));
 window.addEventListener('mousemove', move);
